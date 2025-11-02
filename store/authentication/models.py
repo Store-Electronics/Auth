7d40e920216f22c0d891c4b9d4ab33546d007e35
Saad R <saadrafiqul93380@gmail.com>
2025-11-02 19:27:46 +0100

    Add is_admin if user is admin

--- store/authentication/models.py
+++ store/authentication/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone = models.IntegerField(null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
