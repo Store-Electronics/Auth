40a369ed9caab6d458dd8d41179c105c63a5b7f2
Saad R <saadrafiqul93380@gmail.com>
2025-11-02 22:15:23 +0100

    Add business price

--- store/product/models.py
+++ store/product/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    price_business = models.FloatField(default=0)
     stock = models.IntegerField()
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
